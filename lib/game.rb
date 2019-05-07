6b381c9a50dc842d57073c794e5561c6573f10c8
Andrea Nasuto <andrea.nasuto@gmail.com>
2019-05-07 04:07:15 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -12,18 +12,6 @@ class Game
   [6,4,2]
 ]
 
-  def board
-    board = []
-  end
-
-  def player_1
-    @player_1
-  end
-
-  def player_2
-    @player_2
-  end
-
   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @board = board
     @player_1 = player_1
